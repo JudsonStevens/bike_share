fe6195f6f67891f93178c6e3f2a64640a230e8f6
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-26 17:06:08 -0600

    tested visitor can see Accessory Retired on retired item show page

--- spec/features/visitors/vistor_can_see_retired_item_show_page_spec.rb
+++ spec/features/visitors/vistor_can_see_retired_item_show_page_spec.rb
@@ -5,10 +5,27 @@ describe 'Visitor goes to retired accessory show page' do
     item = Item.create(title:'Bell', price: 1.50, image: 'example.com/image', description: 'Wow, so great!', is_retired?: true )
 
     visit item_path(item)
-    save_and_open_page
 
     expect(page).to have_content(item.title)
     expect(page).to have_content(item.price)
     expect(page).to have_content(item.description)
+  end
+
+  it 'visitor can see retired banner instead of add to cart' do
+    retired = 'Accesory Retired'
+    item = Item.create(title:'Bell', price: 1.50, image: 'example.com/image', description: 'Wow, so great!', is_retired?: true )
+
+    visit item_path(item)
+
+    expect(page).to have_content(retired)
+  end
+
+  it 'visitor cannot see add to cart' do
+    add_to_cart = 'Add to Cart'
+    item = Item.create(title:'Bell', price: 1.50, image: 'example.com/image', description: 'Wow, so great!', is_retired?: true )
+
+    visit item_path(item)
+
+    expect(page).to_not have_content(add_to_cart)
   end
 end
