ed21f20b61ce90ed2fd094fba28761536527ecb6
JamisonOrdway <jamisonordway@gmail.com>
2018-05-26 12:06:07 -0600

    TESTS visitor stations index WIP

---
+++ spec/features/visitor_can_see_stations_index_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Visitor' do
+  context 'they visit the stations index' do
+    it 'should show a list of stations with each stations information' do
+
+    end
+  end
+end
