6e28665808f401fb1aec906313aa799f2d461c12
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-26 10:51:40 -0600

    Implementation of nav bar to accomodate login/logout links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?, :admin?, :authorize
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authorize
+    redirect_to login_path unless current_user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def admin?
+    if !current_user
+      render file: '/public/404.html'
+    elsif !current_user.admin
+      redirect_to root_path
+    end
+  end
 end
