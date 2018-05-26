6feba9b56907c71ca3cf24cdbfd0d6110845043f
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-26 14:25:56 -0600

    removed save and open page from test

--- spec/features/visitors/visitor_visits_trip_show_page_spec.rb
+++ spec/features/visitors/visitor_visits_trip_show_page_spec.rb
@@ -6,7 +6,6 @@ describe 'Visitor visits trip show page' do
     trip = Trip.create(duration: 120, start_date: '2018-05-24 14:40:00' , end_date: '2018-05-24 14:42:01', bike_id: 1, subscription_type: 'Subscriber', end_station_id: station.id, start_station_id: station.id, zip_code:50000 )
 
     visit trip_path(trip)
-    save_and_open_page
 
     expect(page).to have_content(trip.duration)
     expect(page).to have_content(trip.start_date)
