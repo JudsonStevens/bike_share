676486c85905c45c20b2fc8810dca86f1ae5eef8
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-26 14:13:02 -0600

    tested visitor can view trip show with trip attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   resources :stations
   get '/login', "sessions#new"
   post '/login', "sessions#create"
   delete '/logout', "sessions#destroy"
   root "homepage#index"
   resources :users, only: [:new, :create, :index]
+
+  resources :stations, shallow: true do
+    resources :trips, only: [:show]
+  end
+
 end
--- spec/features/visitors/visitor_visits_trip_show_page_spec.rb
+++ spec/features/visitors/visitor_visits_trip_show_page_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 describe 'Visitor visits trip show page' do
   it 'can see a trip and its attributes' do
-    station = Station.create(name:'Turing Station', dock_count: 1, city: 'Denver', datetime:'2018-05-12 01:00:00 UTC')
-    trip = station.trips.create(duration: 120, )
+    station = Station.create(name:'Turing Station', dock_count: 1, city: 'Denver', installation_date:'2018-05-01 01:00:00 UTC')
+    trip = Trip.create(duration: 120, start_date: '2018-05-24 14:40:00' , end_date: '2018-05-24 14:42:01', bike_id: 1, subscription_type: 'Subscriber', end_station_id: station.id, start_station_id: station.id, zip_code:50000 )
+
+    visit trip_path(trip)
+    save_and_open_page
+
+    expect(page).to have_content(trip.duration)
+    expect(page).to have_content(trip.start_date)
+    expect(page).to have_content(trip.end_date)
+    expect(page).to have_content(trip.bike_id)
+    expect(page).to have_content(trip.subscription_type)
   end
 end
