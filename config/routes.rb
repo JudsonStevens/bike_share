e6381496e52c10a20d17a0dbdbc13bd3f416d9b5
JamisonOrdway <jamisonordway@gmail.com>
2018-05-26 17:29:59 -0600

    TESTS Station show WIP

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+  get "/:station-name", to: redirect("/%{station-name}")
   root "homepages#index"
-
   resources :users, only: [:new, :create, :show, :index]
 
   resources :stations, shallow: true do
