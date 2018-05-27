bf7b9045d33f7520b3a17dd5d84cd81989e59375
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-27 11:26:43 -0600

    Working on test for issue 28

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
-  resources :stations
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   root "homepages#index"
+
   resources :users, only: [:new, :create, :index, :show]
+
+  resources :stations
 
   resources :stations, shallow: true do
     resources :trips, only: [:show]
