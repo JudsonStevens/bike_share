1f78831dfb7f0edbe3cfe1cd7178ee85d317b35a
JamisonOrdway <jamisonordway@gmail.com>
2018-05-27 14:26:40 -0600

    visitor trips index wip

--- spec/features/visitors/visitor_can_see_trips_index_spec.rb
+++ spec/features/visitors/visitor_can_see_trips_index_spec.rb
@@ -5,12 +5,15 @@ describe 'Visitor' do
     it 'should show the first 30 trips with all attributes' do
       station_1 = Station.create(name: 'City Park West', dock_count: 5, city: 'Denver', installation_date: '2013-09-21T16:39:57-8:00')
       station_2 = Station.create(name: 'City Park North', dock_count: 4, city: 'Denver', installation_date: '2013-10-21T16:39:57-8:00')
-      trip = Trip.create(duration: 23, start_date: '2014-09-21T16:39:57-8:00', end_date: '2014-09-21T16:55:57-8:00', end_station_id: station_1.id, start_station_id: station_2.id, bike_id: 4, subscription_type: 'gold', zip_code: '08830')
-      trip = Trip.create(duration: 10, start_date: '2014-09-21T16:39:57-8:00', end_date: '2014-09-21T16:49:57-8:00', end_station_id: station_1.id, start_station_id: station_1.id, bike_id: 6, subscription_type: 'gold', zip_code: '78596')
+      trip_1 = Trip.create(duration: 23, start_date: '2014-09-21T16:39:57-8:00', end_date: '2014-09-21T16:55:57-8:00', end_station_id: station_1.id, start_station_id: station_2.id, bike_id: 4, subscription_type: 'gold', zip_code: '08830')
+      trip_2 = Trip.create(duration: 10, start_date: '2014-09-21T16:39:57-8:00', end_date: '2014-09-21T16:49:57-8:00', end_station_id: station_1.id, start_station_id: station_1.id, bike_id: 6, subscription_type: 'gold', zip_code: '78596')
 
 
       visit station_trips_path(station_1)
       save_and_open_page
+
+      expect(page).to have_content(trip_1.duration)
+      expect(page).to have_content(trip_2.duration)
     end
     it 'should have a button to show more trips' do
 
