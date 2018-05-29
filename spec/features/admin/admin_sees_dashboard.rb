101310c3ce29cd707cc3d7498200082e572da50c
JamisonOrdway <jamisonordway@gmail.com>
2018-05-29 08:13:30 -0600

    TESTS 2-Admin-Dashboard

---
+++ spec/features/admin/admin_sees_dashboard.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'Admin' do
+  context 'who is logged in' do
+    it 'should have a link for viewing all accessories' do
+      admin = User.create(first_name: 'Billy', last_name: 'Williams', address:'123 admin street', username:'admin', password: 'admin', role: 1)
+      item1 = Item.create(price: 15.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes', title: 'Bike Limes')
+      item2 = Item.create(price: 22.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x2', title: 'Bike Limes Twice')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_path
+
+      expect(page).to have_link('View all Accessories')
+      click_on 'View all Accessories'
+      expect(current_path).to eq("/admin/bike-shop")
+      expect(page).to have_content(item1.price)
+      expect(page).to have_content(item1.description)
+      expect(page).to have_content(item1.title)
+      expect(page).to have_content(item1.image)
+      expect(page).to have_content(item2.price)
+      expect(page).to have_content(item2.description)
+      expect(page).to have_content(item2.title)
+      expect(page).to have_content(item2.image)
+      expect(page).to have_link("Edit #{item1.title}")
+      expect(page).to have_link("Edit #{item2.title}")
+    end
+  end
+end
