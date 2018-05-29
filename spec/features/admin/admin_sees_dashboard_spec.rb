1cc619de6ac480fb455ce2bf94ca268ab773ca5c
JamisonOrdway <jamisonordway@gmail.com>
2018-05-29 14:12:18 -0600

    2-Admin-Dashboard WIP

--- spec/features/admin/admin_sees_dashboard_spec.rb
+++ spec/features/admin/admin_sees_dashboard_spec.rb
@@ -8,9 +8,9 @@ describe 'Admin' do
       item2 = Item.create(price: 22.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x2', title: 'Bike Limes Twice')
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
+      
       visit admin_dashboards_path(admin)
-      save_and_open_page
+
       expect(page).to have_link('View all Accessories')
       click_on 'View all Accessories'
       expect(current_path).to eq("/admin/bike-shop")
