f36963d98e606e70017707825db20fff724d4058
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-29 19:59:40 -0600

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,22 @@ trips.each do |trip|
   )
 end
 
+User.create(first_name: 'Billy', last_name: 'Williams', address:'123 admin street', username:'admin@gmail.com', password: 'admin', role: 1)
+
+User.create(first_name: 'Logan', last_name: 'X', address: '123 College Way', username: 'logan@gmail.com', password: '123')
+
+Item.create(price: 15.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes', title: 'Bike Limes')
+Item.create(price: 22.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x2', title: 'Bike Limes Twice')
+Item.create(price: 11.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x3', title: 'Bike Limes Thrice')
+Item.create(price: 9.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x4', title: 'Bike Limes Quad')
+Item.create(price: 11.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x5', title: 'Bike Limes Over It')
+Item.create(price: 11.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x6', title: 'Bike Limes Never Again')
+Item.create(price: 16.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x7', title: 'Bike Limes You Got It')
+Item.create(price: 19.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x8', title: 'Bike Limes In Your Face')
+Item.create(price: 825.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x9', title: 'Bike Limes Yup')
+Item.create(price: 8500.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x10', title: 'Bike Limes Nope')
+Item.create(price: 823.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x11', title: 'Bike Limes Dead')
+Item.create(price: 82.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x12', title: 'Bike Limes Alive')
+Item.create(price: 1.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x13', title: 'Bike Limes In Between')
+Item.create(price: 99.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x14', title: 'Bike Limes Yeah')
+Item.create(price: 10.00, image: 'http://i0.kym-cdn.com/entries/icons/original/000/003/980/hold-all-these-limes.jpg', description: 'Too many limes x15', title: 'Bike Limes No')
