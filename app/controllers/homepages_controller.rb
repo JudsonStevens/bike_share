0c8c14c04446e4361770e4200fcbffaaa3a4b3a8
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-31 16:16:27 -0600

    Implementation of user station dashboard

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,17 @@
 class HomepagesController < ApplicationController
   def index
   end
+
+  def show
+    if request.env['PATH_INFO'] == "/stations-dashboard"
+      @station_count = Station.all.count
+      @oldest_station = Station.oldest_station
+      @most_bikes = Station.most_bikes
+      @least_bikes = Station.least_bikes
+      @average_bikes = Station.average_bikes
+      @newest_station = Station.newest_station
+    elsif request.env['PATH_INFO'] == "/trips-dashboard"
+      @trips = Trip.all
+    end
+  end
 end
