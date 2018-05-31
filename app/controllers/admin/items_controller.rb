2c650c716f43f27eec90c2c673f56804fa5cd10c
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-30 20:03:35 -0600

    tested admin can edit items from admin dashboard

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,4 +4,23 @@ class Admin::ItemsController < Admin::BaseController
     @items = Item.all
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      flash[:success] = "You have updated #{item.title}"
+    end
+
+    redirect_to admin_bike_shop_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :price, :image, :description)
+  end
+
 end
