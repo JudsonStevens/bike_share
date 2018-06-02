117dc66efdde624660816545fb3a38232eaad56d
Tyler Westlie <tyler.westlie@gmail.com>
2018-06-02 14:56:18 -0600

    increased price that item can be created with

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
   has_many :orders, through: :item_orders
   validates_presence_of :description
   validates :price, numericality: {greater_than: 0}, presence: true
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: {case_sensitive: false}
 end
--- db/migrate/20180602183448_change_integer_to_decimal_in_item.rb
+++ db/migrate/20180602183448_change_integer_to_decimal_in_item.rb
@@ -1,6 +1,6 @@
 class ChangeIntegerToDecimalInItem < ActiveRecord::Migration[5.2]
   def change
-    remove_column :items, :price
-    add_column :items, :price, :decimal, precision: 8, scale: 2
+    remove_column :items, :price, :integer
+    add_column :items, :price, :decimal, precision: 14, scale: 2
   end
 end
