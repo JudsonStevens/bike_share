11219b5f9f6d164c5edf672af37d7c7087e60bd6
Judson Stevens <Judson.Stevens@gmail.com>
2018-06-02 15:14:34 -0600

    Working on trip dashboard, WIP

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -11,7 +11,18 @@ class HomepagesController < ApplicationController
       @average_bikes = Station.average_bikes
       @newest_station = Station.newest_station
     elsif request.env['PATH_INFO'] == "/trips-dashboard"
-      @trips = Trip.all
+      @average_ride_length = Trip.average_ride_duration
+      @longest_ride = Trip.longest_ride
+      @shortest_ride = Trip.shortest_ride
+      @most_start_station = Trip.most_starting_station
+      @most_end_station = Trip.most_end_station
+      @month_breakdown = Trip.month_breakdown
+      @year_breakdown = Trip.year_breakdown
+      @most_ridden_bike = Trip.most_ridden_bike
+      @least_ridden_bike = Trip.least_ridden_bike
+      total_trips = Trip.all.count
+      require 'pry'; binding.pry
+      @subscription_info = Trip.subscription_count.each { |key, val| return [key, val, ((val/total_trips)*100)]}
     end
   end
 end
---
+++ spec/features/users/user_can_see_trips_dashboard_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'User' do
+  context 'who is logged in' do
+    it 'can see all relevant information on the trips dashboard' do
+      user1 = User.create(first_name: 'Charles', last_name: 'Xavier', username: 'Charles@gmail.com', password: '123', address: '1212 College Lane')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+      
+      station1 = Station.create(name:'Turing Station', dock_count: 2, city: 'Denver', installation_date:'2018-05-01 01:00:00 UTC')
+      station2 = Station.create(name:'G School Station', dock_count: 4, city: 'Denver', installation_date:'2018-05-01 01:00:00 UTC')
+      station3 = Station.create(name:'Red Rock', dock_count: 8, city: 'Denver', installation_date:'2018-05-01 01:00:00 UTC')
+      station4 = Station.create(name:'Holy Heck', dock_count: 3, city: 'Denver', installation_date:'2018-05-01 01:00:00 UTC')
+      trip = Trip.create(duration: 120, start_date: '2018-05-24 14:40:00' , end_date: '2018-05-24 14:42:01', bike_id: 1, subscription_type: 'Subscriber', end_station_id: station2.id, start_station_id: station1.id, zip_code:50000 )
+      trip2 = Trip.create(duration: 150, start_date: '2018-05-26 14:40:00' , end_date: '2018-05-30 14:48:01', bike_id: 3, subscription_type: 'Subscriber', end_station_id: station2.id, start_station_id: station1.id, zip_code:50000 )
+      trip3 = Trip.create(duration: 130, start_date: '2018-05-24 14:40:00' , end_date: '2018-05-24 14:49:01', bike_id: 1, subscription_type: 'Subscriber', end_station_id: station1.id, start_station_id: station3.id, zip_code:50000 )
+      trip4 = Trip.create(duration: 110, start_date: '2018-05-23 14:40:00' , end_date: '2018-05-24 14:43:01', bike_id: 1, subscription_type: 'Subscriber', end_station_id: station4.id, start_station_id: station1.id, zip_code:50000 )
+      trip5 = Trip.create(duration: 190, start_date: '2018-05-22 14:40:00' , end_date: '2018-05-24 14:44:01', bike_id: 2, subscription_type: 'Subscriber', end_station_id: station4.id, start_station_id: station4.id, zip_code:70000 )
+      trip6 = Trip.create(duration: 180, start_date: '2018-05-21 14:40:00' , end_date: '2018-05-24 14:47:01', bike_id: 3, subscription_type: 'Subscriber', end_station_id: station4.id, start_station_id: station2.id, zip_code:60000 )
+
+      visit(trips_dashboard_path)
+
+      # Average Trip Duration
+      expect(page).to have_content(Trip.average(:duration))
+      # Longest Trip
+      expect(page).to have_content(Trip.maximum(:duration))
+      # Shortest Trip
+      expect(page).to have_content(Trip.minimum(:duration))
+      # Station with most rides as a starting station
+      expect(page).to have_content(station1.name)
+      # Station with the most rides as an end station station
+      expect(page).to have_content(station4.name)
+      expect(page).to have_content(trip.bike_id)
+      expect(page).to have_content("3")
+      expect(page).to have_content(trip5.bike_id)
+      expect(page).to have_content(trip.subscription_type)
+      expect(page).to have_content("")
+      expect(page).to have_content()
+      expect(page).to have_content(trip5.bike_id)
+    end
+  end
+end
