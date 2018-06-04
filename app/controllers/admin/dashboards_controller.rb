394ab99bf6e982434190b07b3b30685e0c86535c
JamisonOrdway <jamisonordway@gmail.com>
2018-06-04 11:13:12 -0600

    Finishes functionality for admin dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,17 +1,26 @@
 class Admin::DashboardsController < Admin::BaseController
 
   def show
-    if current_admin?
-      if session[:flash_notice]
-        flash.now[:success] = session[:flash_notice]
-        session[:flash_notice] = nil
-      end
-      @user = User.find(current_user.id)
-      @orders = Order.all
-      flash.now[:success] = session[:flash_notice] if session[:flash_notice]
-      @orders = Order.all
-    else
-      render 'public/404'
+    if session[:flash_notice]
+      flash.now[:success] = session[:flash_notice]
+      session[:flash_notice] = nil
+    end
+    @user = User.find(current_user.id)
+    @orders = Order.all
+    @personal_orders = @user.orders
+    flash.now[:success] = session[:flash_notice] if session[:flash_notice]
+    @ordered = Order.ordered.count
+    @cancelled = Order.cancelled.count
+    @completed = Order.completed.count
+    @paid = Order.paid.count
+    if params[:type] == 'cancelled'
+      @orders = Order.cancelled
+    elsif params[:type] == 'completed'
+      @orders = Order.completed
+    elsif params[:type] == 'ordered'
+      @orders = Order.ordered
+    elsif params[:type] == 'paid'
+      @orders = Order.paid
     end
   end
 end
